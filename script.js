a49a146c5191d8fa532fbdd6cf48ebe8a9054ccf
villavalira <andrea.garcia192@gmail.com>
2026-06-22 11:52:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fbasimport { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
 getAuth,
 GoogleAuthProvider,
