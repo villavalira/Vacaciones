a827cc9ab234e7e721996d07e1797789d29939dc
villavalira <andrea.garcia192@gmail.com>
2026-06-25 12:20:22 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import {
 getFirestore,
 collection,
@@ -21,6 +20,16 @@ appId: "1:977710909114:web:9d2a0c63d67e2a0e60e483",
 measurementId: "G-TNKLJR8W12"
 };
 
+
+const snapshot = await getDocs(q);
+
+console.log("Usuarios pendientes:", snapshot.size);
+
+snapshot.forEach((doc)=>{
+    console.log(doc.id, doc.data());
+});
+
+
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
@@ -38,35 +47,27 @@ if(!lista) return;
 
 lista.innerHTML = "";
 
-
 const q = query(
 collection(db,"users"),
 where("barId","==",null)
 );
 
-
 const snapshot =
 await getDocs(q);
 
-
 snapshot.forEach((userDoc)=>{
-
 const data =
 userDoc.data();
 
 const li =
 document.createElement("li");
 
-
 li.innerHTML = `
-
 <strong>${data.name}</strong>
 <br>
 
 ${data.email}
-
 <br><br>
-
 <button onclick="asignarBar('${userDoc.id}','bar1')">
 🍸 Centro
 </button>
